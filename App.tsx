0be6d8e30683eabfdd49a09cddd6cc62adcea9d5
JuanPaCasRod <juanpacasrod@gmail.com>
2026-01-07 17:50:44 +0100

    Corregir configuración para GitHub Pages

--- App.tsx
+++ App.tsx
@@ -7,8 +7,8 @@ import Home from "./src/pages/Home";
 function Router() {
   return (
     <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/404"} component={NotFound} />
+      <Route path="/" component={Home} />
+      <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/cv.github.io/' : '/',
+  base: '/cv.github.io/',
 })
 
