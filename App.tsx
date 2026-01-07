a9bd05b83a4ae0293a129a750f03da621203fa4e
JuanPaCasRod <juanpacasrod@gmail.com>
2026-01-08 00:58:58 +0100

    Simplify App.tsx to debug routing issues

--- App.tsx
+++ App.tsx
@@ -1,27 +1,7 @@
-import NotFound from "./src/pages/NotFound";
-import { Route, Switch } from "wouter";
-import ErrorBoundary from "./src/components/ErrorBoundary";
-import { ThemeProvider } from "./src/contexts/ThemeContext";
 import Home from "./src/pages/Home";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/404" component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
-  return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="dark">
-        <Router />
-      </ThemeProvider>
-    </ErrorBoundary>
-  );
+  return <Home />;
 }
 
 export default App;
